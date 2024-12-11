3affd8f1b615267f12069bcc4015f672f5a64f7e
Fellipe Vieira <lipesv@gmail.com>
2024-12-11 10:49:59 -0300

    Applied multi-stage build for deploying application using docker compose.

--- mvc1/Program.cs
+++ mvc1/Program.cs
@@ -1,52 +1,46 @@
 using Microsoft.EntityFrameworkCore;
 using mvc1.Models;
 
-internal class Program
+var builder = WebApplication.CreateBuilder(args);
+
+IConfiguration configuration = builder.Configuration;
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<IConfiguration>(configuration);
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddTransient<IRepository, ProdutoRepository>();
+
+// var server = Environment.GetEnvironmentVariable("MYSQL_SERVER");
+var database = Environment.GetEnvironmentVariable("MYSQL_DATABASE");
+var user = Environment.GetEnvironmentVariable("MYSQL_USER");
+var password = Environment.GetEnvironmentVariable("MYSQL_PASSWORD");
+
+var connectionString = $"Server=db;Database={database};User ID={user};Password={password};";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    private static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        IConfiguration configuration = builder.Configuration;
-
-        builder.Services.AddControllersWithViews();
-
-        builder.Services.AddSingleton<IConfiguration>(configuration);
-        builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-        builder.Services.AddTransient<IRepository, ProdutoRepository>();
-
-        var server = Environment.GetEnvironmentVariable("MYSQL_SERVER");
-        var database = Environment.GetEnvironmentVariable("MYSQL_DATABASE");
-        var user = Environment.GetEnvironmentVariable("MYSQL_USER");
-        var password = Environment.GetEnvironmentVariable("MYSQL_PASSWORD");
-
-        var connectionString = $"Server={server};Database={database};User ID={user};Password={password};";
-
-        builder.Services.AddDbContext<AppDbContext>(options =>
-            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
-        );
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-
-        app.Run();
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
